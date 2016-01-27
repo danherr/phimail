fe1055662666142b86b50fc3c01d36984735fd4f
Daniel Herr <dzherr@gmail.com>
2016-01-27 16:49:10 -0500

    Add components for javascript frontend.

---
+++ frontend/pmail.jsx
@@ -0,0 +1,27 @@
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    Router = require('react-router').Router,
+    Route = require('react-router').Route,
+    IndexRoute = require('react-router').IndexRoute,
+    hashHistory = require('history').createHashHistory();
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Javascript is Now Working.
+      </div>
+    );
+  }
+});
+
+var router = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+);
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(router, document.getElementById('root'));
+});
